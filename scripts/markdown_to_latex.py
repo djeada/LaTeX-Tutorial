3f6c5ebb58faea386845dda5ef2877a9ab3a13ba
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-02-14 23:34:33 +0100

    Update markdown_to_latex.py

--- scripts/markdown_to_latex.py
+++ scripts/markdown_to_latex.py
@@ -1,5 +1,24 @@
 import re
 
+def markdown_to_latex_section(section):
+    result = ''
+    for line in section:
+        if line.startswith("##"):
+            result += r'\Large\textbf{' + line[3:].strip() + '}\n'
+        elif line.startswith("#"):
+            result += r'\huge\textbf{' + line[2:].strip() + '}\n'
+        elif line.startswith("-"):
+            result += r'\begin{itemize}\n'
+            result += r'\item ' + line[2:].strip() + '\n'
+            result += r'\end{itemize}\n'
+        elif line.startswith("*"):
+            result += r'\begin{itemize}\n'
+            result += r'\item ' + line[2:].strip() + '\n'
+            result += r'\end{itemize}\n'
+        else:
+            result += line.strip() + '\n'
+    return result
+
 def markdown_to_latex(input_file, output_file):
     with open(input_file, 'r') as f:
         lines = f.readlines()
@@ -18,30 +37,16 @@ def markdown_to_latex(input_file, output_file):
     sections.append(section)
 
     with open(output_file, 'w') as f:
-        f.write(r'\documentclass{beamer}' + '\n')
-        f.write(r'\usepackage{amsmath}' + '\n')
-        f.write(r'\begin{document}' + '\n')
+        f.write(r'\documentclass{beamer}\n')
+        f.write(r'\usepackage{amsmath}\n')
+        f.write(r'\begin{document}\n')
 
         for section in sections:
-            f.write(r'\begin{frame}' + '\n')
-            for line in section:
-                if line.startswith("##"):
-                    f.write(r'\Large\textbf{' + line[3:].strip() + '}' + '\n')
-                elif line.startswith("#"):
-                    f.write(r'\huge\textbf{' + line[2:].strip() + '}' + '\n')
-                elif line.startswith("-"):
-                    f.write(r'\begin{itemize}' + '\n')
-                    f.write(r'\item ' + line[2:].strip() + '\n')
-                    f.write(r'\end{itemize}' + '\n')
-                elif line.startswith("*"):
-                    f.write(r'\begin{itemize}' + '\n')
-                    f.write(r'\item ' + line[2:].strip() + '\n')
-                    f.write(r'\end{itemize}' + '\n')
-                else:
-                    f.write(line.strip() + '\n')
-            f.write(r'\end{frame}' + '\n')
+            f.write(r'\begin{frame}\n')
+            f.write(markdown_to_latex_section(section))
+            f.write(r'\end{frame}\n')
 
-        f.write(r'\end{document}' + '\n')
+        f.write(r'\end{document}\n')
 
 if __name__ == '__main__':
     markdown_to_latex('input.md', 'output.tex')
